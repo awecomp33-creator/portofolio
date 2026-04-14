d41247cd1349b857532ed025dda58f9c29fab3aa
andre <andre@espay.id>
2026-04-14 10:38:46 +0700

    add all code

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,432 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Website Title -->
+    <title>Andre Wijaya - Professional Web Developer</title>
+    <!-- Bootstrap -->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font-Awesome -->
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Lightbox -->
+    <link href="assets/lightbox/css/lightbox.css" rel="stylesheet">
+    <!-- Text Rotator-->
+    <link href="assets/textrotator/simpletextrotator.css" rel="stylesheet">
+    <!-- FlexSlider -->
+    <link href="assets/flexslider/flexslider.css" rel="stylesheet">
+    <!-- Theme Style -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- Animations -->
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom Favicon -->
+    <link href="img/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="assets/html5shiv/html5shiv.js"></script>
+    <script src="assets/respond/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body id="top">
+
+    <!-- ****************************** Preloader ************************** -->
+    <div id="preloader"></div>
+
+
+    <!-- ==========================
+        HEADER SECTION
+        =========================== -->
+    <header id="home">
+        <!-- creative menu -->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="menu-wrap">
+                    <nav class="menu">
+                        <!-- Menu Links -->
+                        <div class="icon-list">
+                            <a href="index.html#home"><i class="fa fa-fw fa-home"></i><span>Home</span></a>
+                            <a href="index.html#about"><i class="fa fa-fw fa-quote-left"></i><span>About</span></a>
+                            <a href="index.html#service"><i class="fa fa-fw fa-globe"></i><span>Service</span></a>
+                            <a href="index.html#portfolio"><i
+                                    class="fa fa-fw fa-picture-o"></i><span>Portfolio</span></a>
+                            <a href="index.html#contact"><i class="fa fa-fw fa-envelope-o"></i><span>Contact</span></a>
+                        </div>
+                    </nav>
+                </div>
+                <button class="menu-button" id="open-button"></button><!-- menu button -->
+            </div><!--/row-->
+        </div><!--/container-->
+        <!-- Header Image -->
+        <section class="hero" id="hero">
+            <div class="container">
+                <!-- Slider Button (don't edit!)-->
+                <div class="row">
+                    <div class="col-md-12 text-right navicon">
+                        <a id="nav-toggle" class="nav_slide_button" href="index.html#"><span></span></a>
+                    </div>
+                </div>
+                <!-- HEADER HEADLINE -->
+                <div class="row">
+                    <div class="col-md-8 inner fullname">
+                        <h1 class="animated fadeInDown">
+                            A<span style="color:#E04343;">nd</span>re<br />
+                            <span>Wi<span style="color:#FFE800">ja</span>ya</span>
+                        </h1><!-- Title -->
+                        <h3 class="animated fadeInUp delay-05s"><span class="rotate">Web Developer</span></h3>
+                        <!-- Text Rotator -->
+                    </div>
+                </div>
+                <!-- Learn More Button -->
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3 text-center">
+                        <a href="index.html#about" class="scrollto wow fadeInUp delay-5s ">
+                            <p>SEE MORE</p>
+                            <p class="scrollto--arrow"><img src="img/scroll-down.png" alt="scroll down arrow"></p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Header Image End -->
+    </header>
+    <!-- ==========================
+        HEADER SECTION END
+        =========================== -->
+
+
+
+
+    <!-- ==========================
+        ABOUT SECTION
+        =========================== -->
+    <section class="intro text-center section-padding color-bg" id="about">
+        <div class="container">
+            <!-- WELCOME TEXT -->
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 wp1">
+                    <h1 class="arrow">A little <span>about</span> me</h1><!-- Headline -->
+                    <!-- about / welcome text -->
+                    <p>I’m a web developer who loves building clean, efficient, and user-focused applications. I enjoy
+                        solving problems, learning new technologies, and turning ideas into functional products.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- ==========================
+        SERVICE SECTION
+        =========================== -->
+    <section class="features text-center section-padding" id="service">
+        <div class="container">
+            <!-- Headline -->
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="arrow">I do amazing things for clients</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="services">
+                        <!-- Service Box 1 -->
+
+                        <!-- Service Box 2 -->
+                        <div class="col-md-12 wp2 item delay-05s">
+                            <div class="icon">
+                                <i class="fa fa-desktop"></i><!-- Icon -->
+                            </div>
+                            <h2>Web Developer</h2><!-- Title -->
+                            <p>I develop high-quality and reliable web solutions designed to support business objectives
+                                and enhance digital presence. By focusing on performance, security, and usability, every
+                                project is built with a professional approach and a clear understanding of each client’s
+                                strategic goals. My commitment is to deliver solutions that are efficient, scalable, and
+                                aligned with corporate standards.</p>
+                        </div>
+
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+    <!-- ==========================
+        ABOUT SECTION END
+        =========================== -->
+
+
+
+
+    <!-- ==========================
+        PORTFOLIO SECTION
+    =========================== -->
+    <section class="swag text-center" id="portfolio">
+        <div class="container">
+            <!-- Headline -->
+            <div class="row">
+                <h1 class="arrow">
+                    My <span>Projects</span>
+                </h1>
+            </div>
+        </div>
+    </section>
+
+    <div class="container">
+        <div class="row row-offset-0">
+
+
+            <div class="row">
+                <div id="blogSlider">
+                    <ul class="slides">
+                        <li>
+                            <!-- Blog Entry 1 -->
+                            <div class="col-md-6 wp4">
+                                <div class="effects clearfix portfolio-thumb">
+                                    <img src="img/portfolio/setiakawan.png" alt="Blog Item">
+                                </div>
+
+                                <br>
+                                <center>
+                                    <h2>Setiakawan Motor</h2>
+                                    <p>Company Profile Website for Setiakawan Group.</p>
+                                </center>
+                            </div>
+                            <div class="col-md-6 wp4">
+                                <div class="effects clearfix portfolio-thumb">
+                                    <img src="img/portfolio/dsl.png" alt="Blog Item">
+                                </div>
+
+                                <br>
+                                <center>
+                                    <h2>DSL Logistik</h2>
+                                    <p>Company Profile Website for PT Daniswara Solusi Logistik (DSL).</p>
+                                </center>
+                            </div>
+                            <div class="col-md-6 wp4">
+                                <div class="effects clearfix portfolio-thumb">
+                                    <img src="img/portfolio/light.png" alt="Blog Item">
+                                </div>
+
+                                <br>
+                                <center>
+                                    <h2>Light Trans Logistic</h2>
+                                    <p>Company Profile Website for Light Trans Logistic.</p>
+                                </center>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div><!--/row-->
+    </div><!--/.container-->
+    <!-- ==========================
+        PORTFOLIO SECTION END
+        =========================== -->
+
+
+
+
+    <!-- ==========================
+        CUSTOM SPACER
+        =========================== -->
+    <div class="spacer-cta text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <a href="index.html#contact" class="outline-btn">hire john doe</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ==========================
+        CUSTOM SPACER END
+        =========================== -->
+
+    <!-- ==========================
+        CONTACT SECTION
+        =========================== -->
+    <section class="text-center section-padding contact-wrap" id="contact">
+        <!-- To Top Button -->
+        <a href="index.html#top" class="up-btn"><i class="fa fa-chevron-up"></i></a>
+        <div class="container">
+            <!-- Headline -->
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="arrow">Drop <span>me</span> a line</h1>
+                </div>
+            </div>
+            <div class="row contact-details">
+                <!-- Adress Box -->
+                <div class="col-md-4">
+                    <div class="dark-box box-hover">
+                        <h2><i class="fa fa-map-marker"></i><span>City</span></h2>
+                        <p>Tangerang</p>
+                    </div>
+                </div>
+                <!-- Phone Number Box -->
+                <div class="col-md-4">
+                    <div class="dark-box box-hover">
+                        <h2><i class="fa fa-mobile"></i><span>Phone</span></h2>
+                        <p>08999790303</p>
+                    </div>
+                </div>
+                <!-- E-Mail Box -->
+                <div class="col-md-4">
+                    <div class="dark-box box-hover">
+                        <h2><i class="fa fa-paper-plane"></i><span>Email</span></h2>
+                        <p><a href="index.html#">andrecomp@gmail.com</a></p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- ==========================
+        CONTACT SECTION END
+        =========================== -->
+
+
+
+    <!-- ==========================
+        FOOTER SECTION
+        =========================== -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p>All Rights Reserved. &copy; <?php echo date('Y'); ?></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ==========================
+        FOOTER SECTION END
+        =========================== -->
+
+
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- SmoothScroll -->
+    <script type="text/javascript" src="assets/smoothscroll/smoothscroll.js"></script>
+    <!-- Bootstrap -->
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Waypoints -->
+    <script src="js/waypoints.min.js"></script>
+    <!-- classie.js -->
+    <script src="js/classie.js"></script>
+    <!-- FlexSlider -->
+    <script src="assets/flexslider/jquery.flexslider.js"></script>
+    <!-- Modernizr -->
+    <script src="js/modernizr.js"></script>
+    <!-- Text Rotator -->
+    <script src="assets/textrotator/jquery.simple-text-rotator.js"></script>
+    <!-- Lightbox.js -->
+    <script src="assets/lightbox/js/lightbox.min.js"></script>
+    <!-- Google Maps -->
+    <script type="text/javascript"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDeKBBPJTG3v5w3cNPAgM6ZsJiPyL1mP_o&amp;sensor=false"></script>
+    <!-- Theme JavaScript Core -->
+    <script src="js/main.js"></script>
+    <script src="js/script.js"></script>
+
+    <!-- GOOGLE MAPS DATA -->
+    <script type="text/javascript">
+        // When the window has finished loading create our google map below
+        google.maps.event.addDomListener(window, 'load', init);
+
+        function init() {
+            // Basic options for a simple Google Map
+            // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+            var mapOptions = {
+                // How zoomed in you want the map to start at (always required)
+                zoom: 15,
+
+                scrollwheel: false,
+
+                // The latitude and longitude to center the map (always required)
+                center: new google.maps.LatLng(40.68961985411178, -74.01618003845215), // New York 
+
+                // How you would like to style the map. 
+                // This is where you would paste any style found on Snazzy Maps.
+                styles: [{
+                    featureType: 'water',
+                    stylers: [{
+                        color: '#F2F2F2'
+                    }, {
+                        visibility: 'on'
+                    }]
+                }, {
+                    featureType: 'landscape',
+                    stylers: [{
+                        color: '#FFFFFF'
+                    }]
+                }, {
+                    featureType: 'road',
+                    stylers: [{
+                        saturation: -100
+                    }, {
+                        lightness: 45
+                    }]
+                }, {
+                    featureType: 'road.highway',
+                    stylers: [{
+                        visibility: 'simplified'
+                    }]
+                }, {
+                    featureType: 'road.arterial',
+                    elementType: 'labels.icon',
+                    stylers: [{
+                        visibility: 'off'
+                    }]
+                }, {
+                    featureType: 'administrative',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#ADADAD'
+                    }]
+                }, {
+                    featureType: 'transit',
+                    stylers: [{
+                        visibility: 'off'
+                    }]
+                }, {
+                    featureType: 'poi',
+                    stylers: [{
+                        visibility: 'off'
+                    }]
+                }]
+            };
+
+            // Get the HTML DOM element that will contain your map 
+            // We are using a div with id="map" seen below in the <body>
+            var mapElement = document.getElementById('googlemaps');
+
+            // Create the Google Map using out element and options defined above
+            var map = new google.maps.Map(mapElement, mapOptions);
+        }
+    </script>
+
+    <!-- TEXT ROTATOR SETTINGS -->
+    <script type="text/javascript">
+        $(".rotate").textrotator({
+            animation: "fade", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+            separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+            speed: 2000 // How many milliseconds until the next word show.
+        });
+    </script>
+
+</body>
+
+</html>
